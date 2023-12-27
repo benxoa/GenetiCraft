b9625e6275f7aa636f8f30c7cb76eebf9c7702e7
benxoa <benxoax@gmail.com>
2023-12-27 17:28:58 +0500

    CORS CXhanges

--- server/server.js
+++ server/server.js
@@ -10,7 +10,15 @@ const path = require('path')
 
 
 app.use(express.json({limit: '50mb'}));
-app.use(cors({origin:"http://47.236.9.48:5173", credentials: true}))
+app.use(cors({
+    origin: [
+      "https://www.geneticraft.fun",
+      "https://geneticraft.fun",
+      "www.geneticraft.fun",
+      "geneticraft.fun",
+    ],
+    credentials: true
+  }));
 const router = require('./router/Router')
 app.use("/api", router)
 
