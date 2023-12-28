5a5936cc823873b4a051b5a55b49b6f9238d9aa3
benxoa <benxoax@gmail.com>
2023-12-29 01:38:09 +0500

    Navbar fix 2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center animate-fade-in-up">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Visualize Your Ideas:
-        <br class="hidden lg:inline-block"/> GenetiCraft Ai Powered Image Generator.
+        <br class="hidden lg:inline-block"/> GenetiCraft Ai Powered Digital Art Generator.
       </h1>
       <p class="mb-8 leading-relaxed animate-fade-in-down">GenetiCraft invites you to explore a world of limitless creativity. Generate stunning images that reflect your imagination. Your canvas awaits!</p>
       <div class="flex justify-center animate-fade-in">
